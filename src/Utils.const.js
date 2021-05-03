533b95b80b9c31eb7886ee3aa46d868e359e9087
Bikash Shrestha <laakhey@Bikashs-MacBook-Pro.local>
2021-05-03 23:40:40 +0545

    updated backend url

--- src/Utils.const.js
+++ src/Utils.const.js
@@ -1,6 +1,6 @@
 const Utils = {
-    // URL : 'https://introcept-backend.herokuapp.com',
-    URL : 'http://localhost:3000',
+    URL : 'https://introcept-backend.herokuapp.com',
+    // URL : 'http://localhost:3000',
     CLIENTS: '/clients',
     DOWNLOAD: '/download'
 }
